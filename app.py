acee797995dbce894dc636cf9492f195a19918cb
asanoshin <litpenguin730822@gmail.com>
2023-06-30 00:42:28 +0800

    /trial

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 line_bot_api = LineBotApi('CFpKo+Ei6jeRbHhKFB6H70Fs806m2HIyydxv0GmqKR5d1kgNtBaf6Dq1vPnIVv10RwrrfNPDMLULyAltA6v0ANkq2a3eFnVHChajvOoJfv1YvGpHqTftBXPjl/PwQYzeRbA/yGxFhrcxNZAlPP07LgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('495877a8a3b6ced6a694c97e969bd231')
 
+@app.route('/')
+def hello():
+    return '這是第一次測試'
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
