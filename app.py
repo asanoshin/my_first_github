4a4d379354d9684fc8cbb09ddca80aee16d87063
asanoshin <litpenguin730822@gmail.com>
2023-06-30 01:20:56 +0800

    config

--- app.py
+++ app.py
@@ -1,31 +1,59 @@
-from flask import Flask
+# 使用系統和 config 檔案的函式庫功能
+import os
+import configparser
+
+# 使用 Flask 的函式庫功能
+from flask import Flask, request, abort
+
+# 使用 LINE Bot SDK 的函式庫功能
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
+
+
+# 設定讀入 config.ini 檔案
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+# Flask Web Service 啟用
 app = Flask(__name__)
 
-from flask import request, abort
-from linebot import  LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+line_bot_api = LineBotApi(config.get('line-bot',
+                                     'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot',
+                                    'channel_secret'))
 
-line_bot_api = LineBotApi('CFpKo+Ei6jeRbHhKFB6H70Fs806m2HIyydxv0GmqKR5d1kgNtBaf6Dq1vPnIVv10RwrrfNPDMLULyAltA6v0ANkq2a3eFnVHChajvOoJfv1YvGpHqTftBXPjl/PwQYzeRbA/yGxFhrcxNZAlPP07LgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('495877a8a3b6ced6a694c97e969bd231')
-
-@app.route('/')
-def hello():
-    return '這是第一次測試'
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
+    # get request body as text
     body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
     try:
+        print(body, signature)
         handler.handle(body, signature)
+        
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
-if __name__ == '__main__':
+    # 從 Line 傳入的訊息
+    msg = event.message.text
+                                               
+    # 回傳相同文字內容
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
+
+
+if __name__ == "__main__":
     app.run()
