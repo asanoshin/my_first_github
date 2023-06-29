dda75527237475330b7a4a9b27aa5edb9308bcdb
asanoshin <litpenguin730822@gmail.com>
2023-06-30 01:38:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 # 使用系統和 config 檔案的函式庫功能
 import os
 import configparser
-import openai
-import tiktoken
 
 # 使用 Flask 的函式庫功能
 from flask import Flask, request, abort
@@ -26,17 +24,12 @@ line_bot_api = LineBotApi(config.get('line-bot',
 handler = WebhookHandler(config.get('line-bot',
                                     'channel_secret'))
 
-
-# openai setting
-os.environ["OPENAI_API_KEY"] = config.get('line-bot',
-                                          'openai_api_key')
 @app.route('/')
 def hello_Flask():
 
-    msg = "這是第二次測式"
+    msg = "Hello, Flask Web Service!"
     
     return msg
-
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -58,55 +51,14 @@ def callback():
     return 'OK'
 
 
-def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
-  
-  try:
-      encoding = tiktoken.encoding_for_model(model)
-  except KeyError:
-      encoding = tiktoken.get_encoding("cl100k_base")
-  if model == "gpt-3.5-turbo-0301":
-      num_tokens = 0
-      for message in messages:
-          num_tokens += 4
-          for key, value in message.items():
-              num_tokens += len(encoding.encode(value))
-              if key == "name":
-                  num_tokens += -1
-      num_tokens += 3  
-      return num_tokens
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
     # 從 Line 傳入的訊息
     msg = event.message.text
-
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
-    messages = [{"role": "user", "content": msg}]
-
-    # 自製的 tokens 計算，用來預先處理 prompt
-    count = num_tokens_from_messages(messages)
-    print(f"{count} prompt tokens counted by num_tokens_from_messages().")
-
-    if count <= 160:
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                messages=messages,
-                                                temperature=0,
-                                                max_tokens=256)
-
-        reply_msg = response.choices[0].message["content"]
-
-        # counting tokens
-        prompt_tokens = response["usage"]["prompt_tokens"]
-        completion_tokes = response["usage"]["completion_tokens"]
-        total_tokens = response["usage"]["total_tokens"]
-        
-    else:
-        reply_msg = "很抱歉，您輸入的提示語句過長，請調整長度"
-
+                                               
     # 回傳相同文字內容
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(reply_msg))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
 
 
 if __name__ == "__main__":
