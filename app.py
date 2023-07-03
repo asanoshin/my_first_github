5718c817b5c89db263b94acd52e076a5eea7892c
asanoshin <litpenguin730822@gmail.com>
2023-07-03 10:45:59 +0800

    chage_TemplateMessage

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def handle_message(event):
                 ]
             )
         )
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, TemplateMessage)
     else:
 
 
